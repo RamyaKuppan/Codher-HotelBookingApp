87e6fd5fcf75c06eeca26a94ef4d60fc46d3bdcb
Mona Lisa <ramyakuppan17@gmail.com>
2019-02-19 19:03:51 +0530

    Created project to display hotel image and name

---
+++ app/src/main/java/com/example/ramya/hotelbookingapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.ramya.hotelbookingapp;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hotel);
+        final Button book = findViewById(R.id.book_button);
+        book.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                book.setText("Booked Successfully");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
